542e08e4e6769fc50fb8e54f97b101a1183429a5
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2024-07-17 16:42:32 +0300

    Task 5 commited

--- Task_5.js
+++ Task_5.js
@@ -1,3 +1,11 @@
+/*Design a function that takes a list of integers as input. The function should
+return True if the list contains two consecutive threes (3 next to a 3) anywhere
+within the list. Otherwise, it should return False. For example, the function
+should return True for [1, 3, 3] and False for [1, 3, 1, 3].
+*/
+
+
+
 const determineDuplicates = (array) => {
     // Sort the array in ascending order to find duplicates
     //lets use the slice & sort functions function ( slice() creates a shallow copy of the array, sort() modifies the original array)
@@ -11,10 +19,13 @@ const determineDuplicates = (array) => {
             if (!result.includes(sortedArray[i])) {
                 // Add the duplicate to the result array, ensuring it's unique (avoid duplicates by checking if it's already in the array)
                 result.push(sortedArray[i]);
+
             }
+        return true;
         }
+
     }
-    return result;
+    return false;
 };
 //new functiom to break the input value into individual integers for proper sorting
 const breakTheInput = (arr) => {
@@ -22,10 +33,12 @@ const breakTheInput = (arr) => {
     return arr.toString().split('').map(Number);
 };
 //initialize an input array
-let arr = [78937897438467626519865];
+let arr1 = [1234567890];
+let arr2 = [987654456789000987654123456789];
 //call the breakTheInput function to convert the input into an array of individual integers
-let newArray = breakTheInput(arr);
-console.log("Broken array:", newArray);
+let newArray1 = breakTheInput(arr1);
+let newArray2 = breakTheInput(arr2);
 //call the Find duplicate function now to confirm the required functionality
-let duplicates = determineDuplicates(newArray);
-console.log("Duplicate values:", duplicates);
+let duplicates = determineDuplicates(newArray2);
+// Should return true if there are duplicates, false otherwise. In this case, there are no duplicates.
+console.log(duplicates);
